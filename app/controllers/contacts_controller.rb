f6bfcdd9f432549e8529f9bda15df9d613441921
Masato Ito <ito-masato@plazacreate.co.jp>
2018-02-25 17:10:21 +0900

    add ActionMailer

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -6,7 +6,7 @@ class ContactsController < ApplicationController
   def create
     @contact = Contact.new(contact_params)
     if @contact.save
-      ContactMailer::contact_mail(@contact).deliver
+      ContactMailer.contact_mail(@contact).deliver
       redirect_to new_contact_path, flash: { info: 'お問い合わせを受け付けました。' }
     else
       render :new
